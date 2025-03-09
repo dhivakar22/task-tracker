199d6c08b9cbbbaac62d51188bbae9bff950c000
dhivakar22 <146105484+dhivakar22@users.noreply.github.com>
2025-03-09 13:29:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import TaskTracker from "./components/TaskTracker"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <TaskTracker />
+    </div>
+  )
+}
+
+export default App
